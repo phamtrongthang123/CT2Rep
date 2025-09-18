d4369e45b2b218b2efcd5d19992963ad3427e77a
Trong Thang Pham <phamtrongthang123@gmail.com>
2025-09-17 19:07:53 -0500

    add downloading script

---
+++ example_download_script/download_only_valid_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from huggingface_hub import hf_hub_download
+from tqdm.auto import tqdm
+
+repo_id = "ibrahimhamamci/CT-RATE"
+directory_name = "dataset/valid_fixed/"
+
+data = pd.read_csv("valid_labels.csv")
+
+for name in tqdm(data["VolumeName"], total=len(data["VolumeName"])):
+    try:
+        folder1 = name.split("_")[0]
+        folder2 = name.split("_")[1]
+        folder = folder1 + "_" + folder2
+        folder3 = name.split("_")[2]
+        subfolder = folder + "_" + folder3
+        subfolder = directory_name + folder + "/" + subfolder
+
+        hf_hub_download(
+            repo_id=repo_id,
+            repo_type="dataset",
+            subfolder=subfolder,
+            filename=name,
+            local_dir="data_volumes",
+        )
+    except:
+        pass
